6c7d26049cdd5c14a078a476ce65ea999234ef0e
Matheus de Paiva Santos <matheus.santos@itafrotas.com>
2025-03-25 11:44:39 -0300

    fix: Change Github Action file 5

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! - Updated V3')
+        'body': json.dumps('Hello from Lambda! - Updated V4')
     }
