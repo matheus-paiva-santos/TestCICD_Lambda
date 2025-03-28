18268c58e0206caff4efb023a65129be00845b8f
Matheus de Paiva Santos <matheus.santos@itafrotas.com>
2025-03-28 10:16:00 -0300

    feat: add psycopg2 files

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,6 +3,7 @@ from utils import run_start_layer_pipeline
 
 def lambda_handler(event, context):
     run_start_layer_pipeline()
+    
     return {
         'statusCode': 200,
         'body': json.dumps({
